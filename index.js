07ace6db60400428438ea2a8d30d8bfb32490695
Meggieweggie <ndirangumegan3@gmail.com>
2025-06-29 13:27:07 +0300

    final

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// index.js
+console.log("DOM editing lab loaded");
+
+// Example DOM code if needed:
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Megan is the champion!';
+document.body.appendChild(newHeader);
+
+const path = "src/index.js";
